90d6e20940dec4652de61d4c9e8612928fa316b4
saikat007saha <saikat007saha@gmail.com>
2016-07-20 18:19:01 +0530

    first commit

--- AddCertificate/AddCertificate/AddCertificate/Form1.Designer.cs
+++ AddCertificate/AddCertificate/AddCertificate/Form1.Designer.cs
@@ -57,13 +57,14 @@
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.LimeGreen;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(153, 196);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(146, 73);
             this.button3.TabIndex = 2;
             this.button3.Text = "Install";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
